88c3c200b054f77f39f8f48ede5c9ae6e50e4442
psg4220 <73107844+psg4220@users.noreply.github.com>
2024-04-09 08:59:02 +0800

    Finished Product

--- app/src/main/java/com/example/vijarprelimexamproject/MainActivity.java
+++ app/src/main/java/com/example/vijarprelimexamproject/MainActivity.java
@@ -3,15 +3,21 @@ package com.example.vijarprelimexamproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button[][] tiles = new Button[5][5];
     private Button restartButton;
+    private TextView displayScore;
     private Match match;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,30 +25,75 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         match = new Match();
         restartButton = findViewById(R.id.buttonRestart);
+        displayScore = findViewById(R.id.displayScore);
         setTiles();
-        restart();
+        setRestart();
 
+        refresh();
     }
 
-    private void setTiles(){
-        int p1, p2;
+    private int firstClickedX = -1;
+    private int firstClickedY = -1;
+
+    private void setTiles() {
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
-                String buttonID = "btn" + (i * 5 + j + 1);
-                int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
-                tiles[i][j] = findViewById(resID);
-                tiles[i][j].setBackgroundColor(Match.intToColor(match.getTile(i,j)));
-                // Set the OnClickListener for this button
+                int buttonId = getResources().getIdentifier("btn" + ((i * 5) + j + 1), "id", getPackageName());
+                tiles[i][j] = findViewById(buttonId);
+
+                final int x = i; // Store the x coordinate
+                final int y = j; // Store the y coordinate
+
                 tiles[i][j].setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
+                        if (firstClickedX == -1 && firstClickedY == -1) {
+                            // Record the first clicked button
+                            firstClickedX = x;
+                            firstClickedY = y;
+                        } else {
+                            // Perform a swap between the first clicked button and the current button
+                            match.swap(firstClickedX, firstClickedY, x, y);
+                            // Reset the first clicked coordinates
+                            firstClickedX = -1;
+                            firstClickedY = -1;
+                            match.replaceMatchedTiles();
+                            setAllTileColor();
+                            displayScore.setText(String.valueOf(match.score()));
+                        }
                     }
                 });
             }
         }
     }
 
-    private void restart() {
+
+    private void setAllTileColor() {
+        for (int i=0;i<5;i++) {
+            for (int j=0;j<5;j++) {
+                setTileColor(i, j, match.getTileColor(i, j));
+            }
+        }
+    }
+
+    private void setTileColor(int x, int y, int colorInt) {
+        tiles[x][y].setBackgroundColor(colorInt);
+    }
+
+    private int getTileColor(int x, int y) {
+        return getBackgroundColor(tiles[x][y]);
+    }
+
+    private int getBackgroundColor(Button button) {
+        Drawable background = button.getBackground();
+        if (background instanceof ColorDrawable) {
+            return ((ColorDrawable) background).getColor();
+        }
+        // Return a default color if the background is not a ColorDrawable
+        return Color.TRANSPARENT;
+    }
+
+    private void setRestart() {
         restartButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -55,10 +106,11 @@ public class MainActivity extends AppCompatActivity {
     private void refresh() {
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
-                tiles[i][j].setBackgroundColor(Match.intToColor(match.getTile(i, j)));
+                tiles[i][j].setBackgroundColor(match.getTileColor(i, j));
             }
         }
-
+        displayScore.setText("0");
+        System.out.println(match);
     }
 
 
--- app/src/main/java/com/example/vijarprelimexamproject/Match.java
+++ app/src/main/java/com/example/vijarprelimexamproject/Match.java
@@ -1,59 +1,62 @@
 package com.example.vijarprelimexamproject;
 
-
 import android.graphics.Color;
 
 import java.util.Random;
 
-interface AbstractMatch{
 
-    int[][] grid = new int[5][5];
-
-    void swap(int x1, int y1, int x2, int y2);
-    boolean isThreeInARow();
-    void replaceMatchedTiles();
-    int score();
-    void restart();
-
-}
-
-public class Match implements AbstractMatch{
-
-    private final int GRID_SIZE = 5;
+public class Match {
+    private int[][] grid;
     private int score;
-    int[][] grid;
-    private Random random;
 
     public Match() {
-        this.grid = new int[GRID_SIZE][GRID_SIZE];
-        this.score = 0;
-        this.random = new Random();
-        this.restart();
+        grid = new int[5][5];
+        score = 0;
+        initializeGrid();
     }
 
-    @Override
-    public void swap(int x1, int y1, int x2, int y2) {
-        // Swap numbers in grid[x1][y1] and grid[x2][y2]
-        int temp = grid[x1][y1];
-        grid[x1][y1] = grid[x2][y2];
-        grid[x2][y2] = temp;
+    private void initializeGrid() {
+        Random rand = new Random();
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                grid[i][j] = rand.nextInt(4) + 1; // Numbers from 1 to 5
+            }
+        }
+        if (this.isThreeInARow()) {
+            initializeGrid();
+        }
     }
 
-    @Override
+    public boolean swap(int x1, int y1, int x2, int y2) {
+        // Check if the positions are adjacent vertically or horizontally
+        if ((x1 == x2 && Math.abs(y1 - y2) == 1) || (y1 == y2 && Math.abs(x1 - x2) == 1)) {
+            int temp = grid[x1][y1];
+            grid[x1][y1] = grid[x2][y2];
+            grid[x2][y2] = temp;
+            if (!this.isThreeInARow()) {
+                temp = grid[x1][y1];
+                grid[x1][y1] = grid[x2][y2];
+                grid[x2][y2] = temp;
+                return false;
+            }
+            return true;
+        } else {
+            System.out.println("Invalid swap! Positions must be adjacent vertically or horizontally.");
+            return false;
+        }
+    }
+
     public boolean isThreeInARow() {
-        // Check if three tiles are lined up (horizontally or vertically)
-        for (int i = 0; i < GRID_SIZE; i++) {
-            for (int j = 0; j < GRID_SIZE; j++) {
-                // Check horizontally
-                if (j + 2 < GRID_SIZE &&
-                        grid[i][j] == grid[i][j + 1] &&
-                        grid[i][j] == grid[i][j + 2]) {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length - 2; j++) {
+                if (grid[i][j] == grid[i][j+1] && grid[i][j] == grid[i][j+2]) {
                     return true;
                 }
-                // Check vertically
-                if (i + 2 < GRID_SIZE &&
-                        grid[i][j] == grid[i + 1][j] &&
-                        grid[i][j] == grid[i + 2][j]) {
+            }
+        }
+        for (int j = 0; j < grid[0].length; j++) {
+            for (int i = 0; i < grid.length - 2; i++) {
+                if (grid[i][j] == grid[i+1][j] && grid[i][j] == grid[i+2][j]) {
                     return true;
                 }
             }
@@ -61,58 +64,64 @@ public class Match implements AbstractMatch{
         return false;
     }
 
-    @Override
     public void replaceMatchedTiles() {
-        // Replace matched tiles with new random numbers
-        for (int i = 0; i < GRID_SIZE; i++) {
-            for (int j = 0; j < GRID_SIZE; j++) {
-                if (isThreeInARow()) {
-                    // Replace matched tiles with random numbers (1 to 4)
-                    Random random = new Random();
-                    grid[i][j] = random.nextInt(4) + 1;
-                    // Update score
-                    score++;
+        Random rand = new Random();
+        boolean replaced;
+
+        do {
+            replaced = false;
+            for (int i = 0; i < grid.length; i++) {
+                for (int j = 0; j < grid[i].length; j++) {
+                    // Check horizontally
+                    int countHorizontal = 1;
+                    while (j + countHorizontal < grid[i].length && grid[i][j] == grid[i][j + countHorizontal]) {
+                        countHorizontal++;
+                    }
+                    if (countHorizontal >= 3) {
+                        for (int k = j; k < j + countHorizontal; k++) {
+                            grid[i][k] = rand.nextInt(4) + 1;
+                        }
+                        this.score += countHorizontal / 3;
+                        replaced = true;
+                    }
+
+                    // Check vertically
+                    int countVertical = 1;
+                    while (i + countVertical < grid.length && grid[i][j] == grid[i + countVertical][j]) {
+                        countVertical++;
+                    }
+                    if (countVertical >= 3) {
+                        for (int k = i; k < i + countVertical; k++) {
+                            grid[k][j] = rand.nextInt(4) + 1;
+                        }
+                        this.score += countVertical / 3;
+                        replaced = true;
+                    }
                 }
             }
-        }
+        } while (replaced);
     }
 
 
-    @Override
     public int score() {
-        return this.score;
+        return score;
     }
 
-    @Override
     public void restart() {
+        initializeGrid();
         this.score = 0;
-        for (int i = 0; i < this.grid.length; i++) {
-            for (int j = 0; j < this.grid[i].length; j++) {
-                this.grid[i][j] = random.nextInt(4) + 1;
+    }
+
+    public void printGrid() {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                System.out.print(grid[i][j] + " ");
             }
+            System.out.println();
         }
     }
 
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < this.grid.length; i++) {
-            for (int j = 0; j < this.grid[i].length; j++) {
-                sb.append(this.grid[i][j]).append(" ");
-            }
-            sb.append("\n");
-        }
-        return sb.toString();
-    }
-
-    public void setTile(int point1, int point2, int value){
-        grid[point1][point2] = value;
-    }
-
-    public int getTile(int point1, int point2) {
-        return grid[point1][point2];
-    }
-
+    // Getter and Setter for grid
     public int[][] getGrid() {
         return grid;
     }
@@ -121,7 +130,25 @@ public class Match implements AbstractMatch{
         this.grid = grid;
     }
 
-    public static int intToColor(int value) {
+    public void setTile(int x, int y, int value){
+        grid[x][y] = value;
+    }
+
+    public void setTileColor(int x, int y, int colorInt) {
+        grid[x][y] = colorToInt(colorInt);
+    }
+
+    public int getTile(int x, int y) {
+        return grid[x][y];
+    }
+
+    public int getTileColor(int x, int y) {
+        return intToColor(grid[x][y]);
+    }
+
+
+
+    private int intToColor(int value) {
         switch (value) {
             case 1:
                 return Color.RED;
@@ -136,7 +163,7 @@ public class Match implements AbstractMatch{
     }
 
 
-    public static int colorToInt(int color) {
+    private int colorToInt(int color) {
         switch (color) {
             case Color.RED:
                 return 1;
@@ -148,6 +175,18 @@ public class Match implements AbstractMatch{
                 return 4;
         }
         return 0;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int[] ints : grid) {
+            for (int anInt : ints) {
+                sb.append(anInt).append(" ");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
     }
 
 }
